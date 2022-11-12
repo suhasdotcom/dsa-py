b1af319926c6b6c5015a03bc80d82ec1309b1c3d
suhas <suhascandoso@gmail.com>
2022-11-13 02:09:15 +0530

    separated out binary-search helper form main function.

--- src/search/binary_search.py
+++ src/search/binary_search.py
@@ -11,18 +11,23 @@ def binary_search(a_iterable, item_to_find):
     :param item_to_find: actual item to find
     :return: index of item if found, -1 if not found
     """
+    low, high, mid = _binary_search_helper(a_iterable, item_to_find)
+    return mid if low < high else -1
 
+
+def _binary_search_helper(a_iterable, item):
     a_iterable_length = len(a_iterable)
     high = a_iterable_length
     low = 0
+    mid = (low + high) // 2
 
     while low < high:
         mid = (low + high) // 2
-        if item_to_find == a_iterable[mid]:
-            return mid
-        elif item_to_find < a_iterable[mid]:
+        if item == a_iterable[mid]:
+            break
+        elif item < a_iterable[mid]:
             high = mid
-        elif item_to_find > a_iterable[mid]:
+        elif item > a_iterable[mid]:
             low = mid
 
-    return -1
+    return low, high, mid
