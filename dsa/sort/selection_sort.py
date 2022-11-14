7996dc022823ebb747defd691234ee68b9772560
suhas <suhascandoso@gmail.com>
2022-11-14 21:18:19 +0530

    Implemented descending sort for selection sort.

--- dsa/sort/selection_sort.py
+++ dsa/sort/selection_sort.py
@@ -3,18 +3,20 @@ Implementation of selection sort by the first principles
 """
 
 
-def selection_sort(a_iterable):
+def selection_sort(a_iterable, descending=False):
     """
     This sort compares the extreme element in a run and places it in one of the extremes
     :param a_iterable: iterable to be sorted
-    :return: an ascending sorted version of this iterable
+    :param descending: Whether the iterable is to be sorted in descending manner, defaults to False
+    :return: sorted version of this iterable
     """
 
     a_iterable_length = len(a_iterable)
 
     for i in range(0, a_iterable_length):
         for j in range(i+1, a_iterable_length):
-            if a_iterable[i] > a_iterable[j]:
+            if (not descending and a_iterable[i] > a_iterable[j])\
+                    or (descending and a_iterable[i] < a_iterable[j]):
                 a_iterable[i], a_iterable[j] = a_iterable[j], a_iterable[i]
 
     return a_iterable
