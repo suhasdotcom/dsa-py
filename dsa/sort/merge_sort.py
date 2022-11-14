698db947fbdd502e326e7d1a40eee3ca3b37a084
suhas <suhascandoso@gmail.com>
2022-11-14 21:31:35 +0530

    Implemented descending sort for merge sort.

--- dsa/sort/merge_sort.py
+++ dsa/sort/merge_sort.py
@@ -3,11 +3,12 @@ Merge sort implementation by the first principles
 """
 
 
-def merge_sort(a_iterable):
+def merge_sort(a_iterable, descending=False):
     """
-    Sorts the iterable using merge sort in ascending order
+    Sorts the iterable using merge sort
 
     :param a_iterable: iterable to be sorted
+    :param descending: Whether the iterable is to be sorted in descending manner, defaults to False
     :return: the sorted iterable
     """
 
@@ -16,17 +17,19 @@ def merge_sort(a_iterable):
     if a_iterable_length < 2:  # stop recursion if none or one element is present as that's already sorted
         return a_iterable
 
-    return merge_iterables(merge_sort(a_iterable[0:a_iterable_length // 2]),
-                           merge_sort(a_iterable[a_iterable_length // 2: a_iterable_length]))
+    return merge_iterables(merge_sort(a_iterable[0:a_iterable_length // 2], descending=descending),
+                           merge_sort(a_iterable[a_iterable_length // 2: a_iterable_length], descending=descending),
+                           descending=descending)
 
 
-def merge_iterables(a_iterable_1, a_iterable_2):
+def merge_iterables(a_iterable_1, a_iterable_2, descending=False):
     """
-    Merges two iterables using finger sorting, ascending currently
+    Merges two iterables using finger sorting
 
     :param a_iterable_1: the first iterable
     :param a_iterable_2: the second iterable
-    :return: ascending merged iterable
+    :param descending: Whether the iterable is to be sorted in descending manner, defaults to False
+    :return: merged iterable
     """
 
     i = 0
@@ -41,7 +44,8 @@ def merge_iterables(a_iterable_1, a_iterable_2):
 
     for counter in range(0, final_new_iterable_length):
         if i < a_iterable_1_length and j < a_iterable_2_length:
-            if a_iterable_1[i] < a_iterable_2[j]:
+            if (not descending and a_iterable_1[i] < a_iterable_2[j])\
+                    or (descending and a_iterable_1[i] > a_iterable_2[j]):
                 new_iterable.append(a_iterable_1[i])
                 i += 1
             else:
