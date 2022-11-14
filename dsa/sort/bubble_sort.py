3e38d2686b24302ba511a41be4318d86a2bd642c
suhas <suhascandoso@gmail.com>
2022-11-14 21:09:41 +0530

    Implemented descending sort for bubble sort.

--- dsa/sort/bubble_sort.py
+++ dsa/sort/bubble_sort.py
@@ -3,19 +3,21 @@ Implementation of bubble sort by first principles
 """
 
 
-def bubble_sort(a_iterable):
+def bubble_sort(a_iterable, descending=False):
     """
-    This sort compares adjacent elements and pushes the heaviest elements to the end of the iterable. As this procedure
-    looks like settling of bubbles hence the name bubble-sort
+    This sort compares adjacent elements and pushes the heaviest elements to the end/beginning of the iterable. As this
+    procedure looks like settling of bubbles hence the name bubble-sort
 
     :param a_iterable: The iterable to be sorted
-    :return: ascending sorted iterable
+    :param descending: Whether the iterable is to be sorted in descending manner, defaults to False
+    :return: sorted iterable
     """
 
     a_iterable_length = len(a_iterable)
     for i in range(0, a_iterable_length-1):
         for j in range(0, a_iterable_length-i-1):
-            if a_iterable[j] > a_iterable[j+1]:
+            if (not descending and a_iterable[j] > a_iterable[j+1]) \
+                    or (descending and a_iterable[j] < a_iterable[j+1]):
                 a_iterable[j], a_iterable[j+1] = a_iterable[j+1], a_iterable[j]
 
     return a_iterable
